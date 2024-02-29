0eaf400d9c757ab493b674935bb5be212baaeaa0
Parijat Purohit <parijat0904114@gmail.com>
2024-02-29 00:42:27 -0700

    add more models for graphql

--- cappuccino/sweetener/admin.py
+++ cappuccino/sweetener/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from sweetener.models import Book, Author
 
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    raw_id_fields = ['author']
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
